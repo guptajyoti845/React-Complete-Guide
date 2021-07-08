92e1d62ea1a37ff11b5c6b40bdaa3e1db5210a24
Jyoti Gupta <Gupta_Jyoti@cat.com>
2021-07-08 23:46:19 +0530

    add User Form and user list render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import AddUser from "./component/User/AddUser"
+import React, {useState} from "react";
+import UserList from "./component/User/UserList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [usersList, setUserList] = useState([]);
+
+    const addUserHandler = (uName, uAge) => {
+        setUserList((prevUsersList) => {
+            return [...prevUsersList, {name: uName, age: uAge}]
+        })
+    }
+    return (
+        <div>
+            <AddUser onAddUser={addUserHandler}/>
+            <UserList users={usersList}/>
+        </div>
+    );
 }
 
 export default App;
